34b2896b700101900614421a99771f45bc0009d5
kimkwatson <kimwatson731@gmail.com>
2025-10-06 21:11:12 -0700

    made navigation dynamic

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -26,9 +27,7 @@ app.set("layout", "layouts/layout")
 app.use(static)
 
 // Index Route
-app.get("/", function(req, res) {
-  res.render("index", {layout: "layouts/layout", title: "Home"})
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
